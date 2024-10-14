da9f04febf20a0c38001d0cef41eb3df1e898e11
RedT0wer <sawitar78@mail.ru>
2024-10-14 17:54:04 +0400

    get and set method

--- ConfigWrite.py
+++ ConfigWrite.py
@@ -7,9 +7,45 @@ class Color:
     stop_color_text = ""
 
 def FilledColor():
-    dictonary = eval(open("Configuration.json","r").readline())
+    dictionary = eval(open("Configuration.json","r").readline())["custom"]
 
-    Color.base_color_text = dictonary["custom"]["base_color_text"]
-    Color.find_color_text = dictonary["custom"]["find_color_text"]
-    Color.difference_color_text = dictonary["custom"]["difference_color_text"]
-    Color.stop_color_text = dictonary["custom"]["stop_color_text"]
+    Color.base_color_text = dictionary["base_color_text"]
+    Color.find_color_text = dictionary["find_color_text"]
+    Color.difference_color_text = dictionary["difference_color_text"]
+    Color.stop_color_text = dictionary["stop_color_text"]
+
+
+def GetFontSize():
+    fil = open("Configuration.json","r")
+    dictionary = eval(fil.readline())["custom"]
+    fil.close()
+    return dictionary["font_size"]
+
+def SetFontSize(new_font_size):
+    fil = open("Configuration.json","r")
+    dictionary = eval(fil.readline())["custom"]
+    fil.close()
+
+    dictionary["font_size"] = new_font_size
+
+    fil = open("Configuration.json","w")
+    fil.write(json.dumps(dictionary))
+    fil.close()
+
+
+def GetPath():
+    fil = open("Configuration.json","r")
+    dictionary = eval(fil.readline())["path_folder_cache"]
+    fil.close()
+    return dictionary["path_folder_cache"]
+
+def SetPath(new_path_folder_cache):
+    fil = open("Configuration.json","r")
+    dictionary = eval(fil.readline())["path_folder_cache"]
+    fil.close()
+
+    dictionary["path_folder_cache"] = new_path_folder_cache
+
+    fil = open("Configuration.json","w")
+    fil.write(json.dumps(dictionary))
+    fil.close()

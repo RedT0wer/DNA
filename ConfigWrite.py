f4b2024f31df5ed5cd575fc9dfaa609b95f4e24b
RedT0wer <sawitar78@mail.ru>
2024-10-15 18:27:08 +0400

    changed

--- ConfigWrite.py
+++ ConfigWrite.py
@@ -1,63 +1,30 @@
 import json
 
-class Color:
-    base_color_text = "",
-    find_color_text = "",
-    difference_color_text = "",
-    stop_color_text = ""
+class Config:
+    def __init__(self):
+        json_data = open("Configuration.json","r")
+        dictionary = json.load(json_data)
+        self.__dict__ = dictionary['custom']
+        json_data.close()
 
-def FilledColor():
-    dictionary = eval(open("Configuration.json","r").readline())["custom"]
+    def return_default(self):
+        json_data = open("Configuration.json","r")
+        dictionary = json.load(json_data)
+        json_data.close()
+        
+        dictionary["custom"] = dictionary["default"]
 
-    Color.base_color_text = dictionary["base_color_text"]
-    Color.find_color_text = dictionary["find_color_text"]
-    Color.difference_color_text = dictionary["difference_color_text"]
-    Color.stop_color_text = dictionary["stop_color_text"]
+        json_data = open("Configuration.json","w")
+        json_data.write(json.dumps(dictionary))
+        json_data.close()
 
+    def apply_settings(self):
+        json_data = open("Configuration.json","r")
+        dictionary = json.load(json_data)
+        json_data.close()
+        
+        dictionary["custom"] = self.__dict__
 
-def GetFontSize():
-    fil = open("Configuration.json","r")
-    dictionary = eval(fil.readline())["custom"]
-    fil.close()
-    return dictionary["font_size"]
-
-def SetFontSize(new_font_size):
-    fil = open("Configuration.json","r")
-    dictionary = eval(fil.readline())["custom"]
-    fil.close()
-
-    dictionary["font_size"] = new_font_size
-
-    fil = open("Configuration.json","w")
-    fil.write(json.dumps(dictionary))
-    fil.close()
-
-
-def GetPath():
-    fil = open("Configuration.json","r")
-    dictionary = eval(fil.readline())["custom"]
-    fil.close()
-    return dictionary["path_folder_cache"]
-
-def SetPath(new_path_folder_cache):
-    fil = open("Configuration.json","r")
-    dictionary = eval(fil.readline())["custom"]
-    fil.close()
-
-    dictionary["path_folder_cache"] = new_path_folder_cache
-
-    fil = open("Configuration.json","w")
-    fil.write(json.dumps(dictionary))
-    fil.close()
-
-
-def ReturnDefault():
-    fil = open("Configuration.json","r")
-    dictionary = eval(fil.readline())
-    fil.close()
-
-    dictionary["custom"] = dictionary["default"]
-
-    fil = open("Configuration.json","w")
-    fil.write(json.dumps(dictionary))
-    fil.close()
+        json_data = open("Configuration.json","w")
+        json_data.write(json.dumps(dictionary))
+        json_data.close()
